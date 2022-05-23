4e7446b90a622712c14aee09cf22395ca7498b12
time_to_travel <45129156@certus.edu.pe>
2022-05-23 18:11:35 -0500

    angular app

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ const port = process.env.PORT;
 
 app.use(express.static('public'));
 
-app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
-});
-app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
-});
+
+
+
+
+
 app.get('*', (req, res) => {
     //res.send('404 | Page not found');
-    res.sendFile(__dirname + '/public/404.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 app.listen(port, () => {
     console.log(`La aplicacion esta corriendo en el puerto ${port}`)
